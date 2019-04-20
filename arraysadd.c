a3a3e11b8963de767485da2fc6580d9f9f8b8fd1
yashna1 <49425003+yashna1@users.noreply.github.com>
2019-04-20 12:02:35 +0530

    Create arraysadd.c

---
+++ arraysadd.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int input()
+{
+int n;
+printf("enter the number of array elements:");
+scanf("%d",&n);
+return n;
+}
+int input1()
+{
+int i,a[i],n;
+printf("enter the array elements:=%d",&a[i]);
+for(i=0;i<n;i++)
+scanf("%d",&a[i]);
+return a[i];
+}
+void output(int a[i])
+{
+printf("the array elements are:=%d",a[i]);
+}
+int main()
+{
+n=input();
+a[i]=input1();
+output(a[i]);
+}
