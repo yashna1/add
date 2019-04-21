cc379827ffad7a3d6f7e9631c9f8ba6223a0178f
yashna1 <49425003+yashna1@users.noreply.github.com>
2019-04-21 22:05:56 +0530

    Update arraysadd.c

--- arraysadd.c
+++ arraysadd.c
@@ -6,12 +6,14 @@ printf("enter the number of array elements:");
 scanf("%d",&n);
 return n;
 }
-int input1()
+int input1(int n)
 {
-int i,a[i],n;
-printf("enter the array elements:=%d",&a[i]);
+int i,a[i];
 for(i=0;i<n;i++)
+{
+printf("enter the array elements:=%d",&a[i]);
 scanf("%d",&a[i]);
+}
 return a[i];
 }
 void output(int a[i])
@@ -21,6 +23,7 @@ printf("the array elements are:=%d",a[i]);
 int main()
 {
 n=input();
-a[i]=input1();
-output(a[i]);
+int n,a[i];
+input1(a);
+output(a);
 }
